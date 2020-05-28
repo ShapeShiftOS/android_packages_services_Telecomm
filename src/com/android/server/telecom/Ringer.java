1b05ba75559993e3092e913f0b3f5c5abc3acb9a
madeofgreat <ravenklawasd@gmail.com>
2020-05-28 15:41:22 +0530

    Revert "Telecomm: Incall vibration option [2/3]"
    This reverts commit 159d43c9e7b831cbcb6ad6b279483cf6acd6b19c.

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -31,10 +31,8 @@ import android.media.VolumeShaper;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.provider.Settings;
 
 import com.android.internal.annotations.VisibleForTesting;
 
@@ -175,12 +173,7 @@ public class Ringer {
         255, // Vibrate full amplitude
         0, // No amplitude while waiting
     };
-
-    private static final long[] CALL_WAITING_VIBRATION_PATTERN = {
-            200,
-            300,
-            500,
-    };
+    
     private boolean mUseSimplePattern;
     private int mVibrationPattern;
     private SettingsObserver mSettingObserver;
@@ -518,13 +511,6 @@ public class Ringer {
         Log.v(this, "Playing call-waiting tone.");
 
         stopRinging();
-
-       if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.INCALL_FEEDBACK_VIBRATE, 0, UserHandle.USER_CURRENT) == 1) {
-            if (mVibrator.hasVibrator()) {
-                mVibrator.vibrate(CALL_WAITING_VIBRATION_PATTERN, -1);
-            }
-        }
 
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
